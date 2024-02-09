ee5b5c662163ea88dc38be86bfa0bca7ca5883ad
Arnold Messifet <arnold.messifet@nomadstudio.ch>
2024-02-09 06:59:28 +0100

    setup firebase features

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,48 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, InjectionToken } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environments/environment';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import {
+  Firestore,
+  initializeFirestore,
+  connectFirestoreEmulator,
+  getFirestore,
+} from 'firebase/firestore';
+
+const app = initializeApp(environment.firebase);
+
+export const AUTH = new InjectionToken('Firebase auth', {
+  providedIn: 'root',
+  factory: () => {
+    const auth = getAuth();
+    if (environment.useEmulators) {
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+    }
+    return auth;
+  },
+});
+
+export const FIRESTORE = new InjectionToken('Firebase firestore', {
+  providedIn: 'root',
+  factory: () => {
+    let firestore: Firestore;
+
+    if (environment.useEmulators) {
+      firestore = initializeFirestore(app, {});
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+    } else {
+      firestore = getFirestore();
+    }
+    return firestore;
+  },
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync()],
 };
